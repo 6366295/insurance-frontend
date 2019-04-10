460f6420bb22a71346a5d7fca6278057ed543deb
Mike Trieu <mt_tr1pl@hotmail.com>
2019-04-11 00:59:37 +0200

    Trying out SASS, PUG. Also tried making calls to the insurance-rest-api

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,42 @@
 var express = require('express');
 var router = express.Router();
 
+var http = require('http');
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/', function(req, res, next) {
+  // form
+  var post = req.body;
+
+  var options = {
+    host: "localhost",
+    port: "8080",
+    path: "/customers",
+    method: "GET",
+    headers: {
+        "Content-Type": "application/json"
+    }
+  };
+
+  var req2 = http.request(options, function (res) {
+    var responseString = "";
+
+    res.on("data", function (data) {
+        responseString += data;
+        // save all the data from response
+    });
+    res.on("end", function () {
+        console.log(responseString);
+        // print to console when response ends
+    });
+  });
+
+  req2.write("TEST");
+
+  req2.end()
+
+  res.send(post);
+  // console.error(name);
 });
 
 module.exports = router;
