426de4829cd426aff1bd076e006fbf43e3ed107d
Mike Trieu <mt_tr1pl@hotmail.com>
2019-04-29 23:00:43 +0200

    processing post response

--- routes/applied.js
+++ routes/applied.js
@@ -9,12 +9,11 @@ router.post('/', function(req, res, next) {
 
   _.postMethod(body, function(responseBody) {
       var responseJson = JSON.parse(responseBody);
-      console.log(JSON.stringify(responseJson['details']));
 
       if (responseJson['code'] == '422') {
         res.render('index', { title: 'Invalid Fields', response: JSON.stringify(responseJson['details'])});
       } else {
-        res.render('index', { title: 'Insurance Application Sent' });
+        res.render('sent', { title: 'Insurance Application Sent' });
       }
   });
 
